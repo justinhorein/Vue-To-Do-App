cd66e29d801dcb7c5c8cc6e1d494008401399898
Justin Horein <justinhorein@gmail.com>
2021-08-31 14:06:43 -0700

    add form, bind method to submit event, bin data to input

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form @submit="onSubmit" @submit.prevent="onSubmit">
+        <label 
+        for="new-todo-input">
+            What needs to be done?
+        </label>
+        <input
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+            v-model="label"
+        />
+        <button type="submit">
+            Add 
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    methods: {
+        onSubmit() {
+            console.log(this.label)
+        }
+    },
+    data() {
+        return {
+            label: ""
+        };
+    }
+};
+</script>
